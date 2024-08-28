6dc2108a15b1e78390e2fa0e7f4284dcc8393ab2
Justas Furmonas <justasfurmonas@gmail.com>
2024-08-28 18:13:04 +0300

    refactor: moved sub_arguments inside task impl

--- src/cli.rs
+++ src/cli.rs
@@ -2,27 +2,24 @@ use std::fmt::Error;
 
 use crate::tasks::*;
 
-pub fn parse_cmd() -> Result<&'static dyn CommandProc, Error> {
+pub fn parse_cmd() -> Result<Box<dyn CommandProc>, Error> {
     let command = std::env::args().nth(1);
+    let sub_arguments: Vec<String> = std::env::args().skip(2).collect();
 
     match command {
-        Some(command) => find_command(command),
+        Some(command) => find_command(command, sub_arguments),
         None => Err(Error),
     }
 }
 
-pub fn parse_sub_arguments() -> Vec<String> {
-    std::env::args().skip(2).collect()
-}
-
-fn find_command(command: String) -> Result<&'static dyn CommandProc, Error> {
+fn find_command(command: String, sub_arguments: Vec<String>) -> Result<Box<dyn CommandProc>, Error> {
     match command.as_str() {
-        "add" => Ok(&AddCmd),
-        "update" => Ok(&UpdateCmd),
-        "delete" => Ok(&DeleteCmd),
-        "mark-in-progress" => Ok(&MarkProgressCmd),
-        "mark-done" => Ok(&MarkDoneCmd),
-        "list" => Ok(&ListCmd),
+        "add" => Ok(Box::new(AddCmd {sub_arguments} )),
+        "update" => Ok(Box::new(UpdateCmd {sub_arguments} )),
+        "delete" => Ok(Box::new(DeleteCmd {sub_arguments} )),
+        "mark-in-progress" => Ok(Box::new(MarkProgressCmd {sub_arguments} )),
+        "mark-done" => Ok(Box::new(MarkDoneCmd {sub_arguments} )),
+        "list" => Ok(Box::new(ListCmd {sub_arguments} )),
         _ => Err(Error),
     }
 }
--- src/tasks.rs
+++ src/tasks.rs
@@ -4,57 +4,57 @@ pub trait CommandProc {
     fn process_command(&self);
 }
 
-pub struct AddCmd;
-pub struct UpdateCmd;
-pub struct DeleteCmd;
-pub struct MarkProgressCmd;
-pub struct MarkDoneCmd;
-pub struct ListCmd;
+pub struct AddCmd {
+    pub sub_arguments: Vec<String>
+}
+pub struct UpdateCmd {
+    pub sub_arguments: Vec<String>
+}
+pub struct DeleteCmd {
+    pub sub_arguments: Vec<String>
+}
+pub struct MarkProgressCmd {
+    pub sub_arguments: Vec<String>
+}
+pub struct MarkDoneCmd {
+    pub sub_arguments: Vec<String>
+}
+pub struct ListCmd {
+    pub sub_arguments: Vec<String>
+}
 
 impl CommandProc for AddCmd {
     fn process_command(&self) {
-        let sub_arguments: Vec<String> = parse_sub_arguments();
-
-        println!("Add command received args -> {:?}", sub_arguments);
+        println!("Add command received args -> {:?}", self.sub_arguments);
     }
 }
 
 impl CommandProc for UpdateCmd {
     fn process_command(&self) {
-        let sub_arguments: Vec<String> = parse_sub_arguments();
-
-        println!("Update command received args -> {:?}", sub_arguments);
+        println!("Update command received args -> {:?}", self.sub_arguments);
     }
 }
 
 impl CommandProc for DeleteCmd {
     fn process_command(&self) {
-        let sub_arguments: Vec<String> = parse_sub_arguments();
-
-        println!("Delete command received args -> {:?}", sub_arguments);
+        println!("Delete command received args -> {:?}", self.sub_arguments);
     }
 }
 
 impl CommandProc for MarkProgressCmd {
     fn process_command(&self) {
-        let sub_arguments: Vec<String> = parse_sub_arguments();
-
-        println!("Mark-in-progress command received args -> {:?}", sub_arguments);
+        println!("Mark-in-progress command received args -> {:?}", self.sub_arguments);
     }
 }
 
 impl CommandProc for MarkDoneCmd {
     fn process_command(&self) {
-        let sub_arguments: Vec<String> = parse_sub_arguments();
-
-        println!("Mark-done command received args -> {:?}", sub_arguments);
+        println!("Mark-done command received args -> {:?}", self.sub_arguments);
     }
 }
 
 impl CommandProc for ListCmd {
     fn process_command(&self) {
-        let sub_arguments: Vec<String> = parse_sub_arguments();
-
-        println!("List command received args -> {:?}", sub_arguments);
+        println!("List command received args -> {:?}", self.sub_arguments);
     }
 }
